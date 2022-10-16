b25f97279314fb1dde606205b4cb597121ae9177
n80267av <andrej.velichkovski@student.manchester.ac.uk>
2022-10-16 17:32:18 +0100

    Add one CLI group for all spawners

---
+++ benchmark_cli/top_level_spawner.py
@@ -0,0 +1,23 @@
+# Top level CLI group
+# All other spawner commands belong to this group
+import click
+
+@click.group(help="CLI tool for spawning lightweight virtualization systems")
+@click.option(
+    '-i', '--instances',
+    type=int,
+    help='Number of instances to spawn as part of the experiment',
+    required=True,
+)
+@click.option(
+    '-n', '--name',
+    type=str,
+    help='Name of image to use',
+    required=True,
+)
+@click.pass_context
+def spawner(ctx, instances, name):
+	ctx.ensure_object(dict)
+
+	ctx.obj['instances'] = instances
+	ctx.obj['name'] = name
