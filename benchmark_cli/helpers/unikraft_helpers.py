94e92628da30c89e10f1d6d031c8b9e46a9ed1ef
n80267av <andrej.velichkovski@student.manchester.ac.uk>
2022-10-20 19:15:01 +0100

    Update Unikraft to support redis and nginx

---
+++ benchmark_cli/helpers/unikraft_helpers.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+
+def _run_qemu_network(ip_address, path, kernel_name, conf_file=""):
+    command = f"""
+        sudo qemu-system-x86_64 -fsdev local,id=myid,path=$(pwd)/fs0,security_model=none \
+            -device virtio-9p-pci,fsdev=myid,mount_tag=fs0,disable-modern=on,disable-legacy=off \
+            -netdev bridge,id=en0,br=virbr0 \
+            -device virtio-net-pci,netdev=en0 \
+            -kernel "{kernel_name}" \
+            -append "-M 1024 netdev.ipv4_addr={ip_address} netdev.ipv4_gw_addr=172.16.0.1 netdev.ipv4_subnet_mask=255.255.255.0 -- {conf_file}" \
+            -cpu host \
+            -enable-kvm \
+            -daemonize \
+            -display none
+    """
+    p = subprocess.Popen(command, shell=True, cwd=path)
+
+def _run_redis_instance(ip_address, instance_cnt):
+    pwd = os.getcwd()
+    redis_pwd = pwd + "/unikraft-redis/apps/app-redis"
+    os.environ["UK_WORKDIR"] = redis_pwd + "/../../"
+
+    _run_qemu_network(
+        ip_address=ip_address,
+        path=redis_pwd,
+        kernel_name="build/redis_kvm-x86_64",
+        conf_file=f"/redis{instance_cnt}.conf",
+    )
+
+def _run_nginx_instance(ip_address):
+    pwd = os.getcwd()
+    nginx_pwd = pwd + "/unikraft-nginx/apps/app-nginx"
+    os.environ["UK_WORKDIR"] = nginx_pwd + "/../../"
+
+    _run_qemu_network(
+        ip_address=ip_address,
+        path=nginx_pwd,
+        kernel_name="build/nginx_kvm-x86_64",
+    )
+
+def run_unikraft(ip_address, instance_cnt, name):
+    if name == "nginx":
+        _run_nginx_instance(ip_address)
+    elif name == "redis":
+        _run_redis_instance(ip_address, instance_cnt)
--- benchmark_cli/unikraft_spawner.py
+++ benchmark_cli/unikraft_spawner.py
@@ -10,6 +10,8 @@ import os
 
 import subprocess
 
+from helpers.unikraft_helpers import run_unikraft
+
 log.basicConfig(
     level=log.INFO, filename="/dev/stdout",
     format="%(levelname)s: %(message)s"
@@ -20,30 +22,13 @@ def setup_network(ips_required):
     subprocess.run("sudo ip link del dev virbr0", shell=True)
 
     subprocess.run("sudo ip link add virbr0 type bridge", shell=True)
+    subprocess.run("tc qdisc add dev virbr0 root netem delay 0ms", shell=True)
+    
     for i in range(ips_required):
         subprocess.run(f"sudo ip address add 172.{16 + i}.0.1/24 dev virbr0", shell=True)
 
     subprocess.run("sudo ip link set dev virbr0 up", shell=True)
 
-def run_single_instance(ip_address):
-    pwd_path = "/home/andrej/assessing-scalability/benchmark_cli/unikraft-nginx/apps/app-nginx"
-    os.environ["UK_WORKDIR"] = pwd_path + "/../../"
-
-    command = f"""
-        sudo qemu-system-x86_64 -fsdev local,id=myid,path=$(pwd)/fs0,security_model=none \
-            -device virtio-9p-pci,fsdev=myid,mount_tag=fs0,disable-modern=on,disable-legacy=off \
-            -netdev bridge,id=en0,br=virbr0 \
-            -device virtio-net-pci,netdev=en0 \
-            -kernel "build/nginx_kvm-x86_64" \
-            -append "-M 1024 netdev.ipv4_addr={ip_address} netdev.ipv4_gw_addr=172.16.0.1 netdev.ipv4_subnet_mask=255.255.0.0 --" \
-            -cpu host \
-            -enable-kvm \
-            -daemonize \
-            -display none
-    """
-
-    p = subprocess.Popen(command, shell=True, cwd=pwd_path)
-    
 def unikraft_spawner(instances, name):
     if instances % 10 != 0:
         log.error("Invalid Input: Please enter instances as number divisible by 10")
@@ -54,9 +39,10 @@ def unikraft_spawner(instances, name):
     INSTANCES_PER_IP = 200
     ips_required = (instances + INSTANCES_PER_IP - 1) // INSTANCES_PER_IP
 
+    log.info(os.getcwd())
+
+
     setup_network(ips_required)
-
-
     active_vms = 0
 
     for i in range(ips_required):
@@ -65,13 +51,16 @@ def unikraft_spawner(instances, name):
                 break
 
             current_ip = f"172.{16 + i}.0.{2 + j}"
-            run_single_instance(current_ip)
+            run_unikraft(
+                ip_address=current_ip,
+                instance_cnt=active_vms+1,
+                name=name,
+            )
             active_vms += 1
 
             # Print benchmark progress
             if active_vms % (instances // 10) == 0:
                 log.info(f"Benchmark progress: { (active_vms // (instances // 10)) * 10 }%")
-
 
 if __name__ == '__main__':
     unikraft_spawner()
