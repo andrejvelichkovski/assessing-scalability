1083b9223da98dffcc43a2166d43e0b90fb74d34
n80267av <andrej.velichkovski@student.manchester.ac.uk>
2023-03-21 11:21:02 +0000

    Update fork attack to use infinite forks

--- resources/common/fork-bomb/fork-bomb.c
+++ resources/common/fork-bomb/fork-bomb.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <sys/types.h>
- 
+#include <stdlib.h>
+
 int main()
 {
     while(1)
     {
-        fork();   
+        int child_id = fork();
+        if(child_id > 0) {
+            wait(NULL);
+        }
+    	else if (child_id==0){
+		    exit(0);
+	    }
+	    else {
+	    	printf("ERROR %d\n", child_id);
+	    	exit(1);
+	    }
     }
     return 0;
 }
