4b6a1651f9977b3f415243a981543cf0c7ab6552
n80267av <andrej.velichkovski@student.manchester.ac.uk>
2023-03-21 12:42:56 +0000

    Update docker form bomb attack code

--- resources/docker_images/fork-bomb/fork-bomb.c
+++ resources/docker_images/fork-bomb/fork-bomb.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <stdlib.h>
 
 int main()
 {
     while(1)
     {
-	printf("starting new system\n");
-    	fork();
-    	sleep(0.1);
+        int child_id = fork();
+        if(child_id > 0) {
+            wait(NULL);
+        }
+    	else if (child_id==0){
+		    exit(0);
+	    }
+	    else {
+	    	printf("ERROR %d\n", child_id);
+	    	exit(1);
+	    }
     }
     return 0;
 }
