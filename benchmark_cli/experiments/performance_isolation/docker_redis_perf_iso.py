8f808e7d734631e7746bd73ad3ec2dd1b84add9d
n80267av <andrej.velichkovski@student.manchester.ac.uk>
2023-04-06 12:09:49 +0100

    Update Redis perf iso experiments

--- benchmark_cli/experiments/performance_isolation/docker_redis_perf_iso.py
+++ benchmark_cli/experiments/performance_isolation/docker_redis_perf_iso.py
@@ -5,16 +5,26 @@ from helpers.docker_helpers import create_container_taskset, \
     clean_all_containers, create_container_taskset_static
 from helpers.redis_benchmark_helpers import run_redis_benchmark
 EXPERIMENT_NAME = "d_redis_perf_iso"
-SAME_CORE = 1
 BUSY_CONTAINER_NAME = "busy-container"
 
+MAIN_CORE = 27
+SAME_CORE = 79
+SAME_CPU = 85
+SAME_MACHINE = 15
 
-def run_two_containers(taskset_1, taskset_2, file_name, run_index):
-    create_container_taskset(port_in=6379, port_out=8080, image_name="redis-benchmark", taskset_text=taskset_1)
-    create_container_taskset_static(image_name=BUSY_CONTAINER_NAME, taskset_text=taskset_2)
+def run_two_containers(main_taskset, attack_taskset, file_name, run_index, instances_per_benchmark):
+    create_container_taskset(port_in=6379, port_out=8080, image_name="redis-benchmark", taskset_text=main_taskset)
+    create_container_taskset_static(image_name=BUSY_CONTAINER_NAME, taskset_text=attack_taskset)
+    time.sleep(5)
 
-    time.sleep(25)
-    run_redis_benchmark("localhost", "8080", f"./benchmark-data/{EXPERIMENT_NAME}/{run_index}-{file_name}.out")
+    for i in range(instances_per_benchmark):
+        log.info(f"./benchmark-data/{EXPERIMENT_NAME}/{(run_index - 1) * instances_per_benchmark+ i}-{file_name}.out")
+        run_redis_benchmark(
+            "localhost",
+            "8080",
+            f"./benchmark-data/{EXPERIMENT_NAME}/{(run_index - 1) * instances_per_benchmark + i}-{file_name}.out"
+        )
+        time.sleep(1)
 
 
 def run_docker_redis_perf_iso_experiment(run_index, benchmark_times, instances_per_benchmark):
@@ -22,19 +32,35 @@ def run_docker_redis_perf_iso_experiment(run_index, benchmark_times, instances_p
         port_in=6379,
         port_out=8080,
         image_name="redis-benchmark",
-        taskset_text=f"taskset {SAME_CORE}"
+        taskset_text=f"taskset {MAIN_CORE}"
     )
     time.sleep(10)
 
     log.info("Main container started. Starting first wrk benchmark")
 
-    run_redis_benchmark("localhost", "8080", f"./benchmark-data/{EXPERIMENT_NAME}/{run_index}-single.out")
+    for i in range(instances_per_benchmark):
+        log.info(f"./benchmark-data/{EXPERIMENT_NAME}/{(run_index - 1) * instances_per_benchmark + i}-single.out")
+        run_redis_benchmark(
+            "localhost",
+            "8080",
+            f"./benchmark-data/{EXPERIMENT_NAME}/{(run_index - 1) * instances_per_benchmark + i}-single.out"
+        )
+        time.sleep(1)
+
     log.info("Single run completed")
 
     clean_all_containers()
-    time.sleep(10)
+    time.sleep(5)
 
     log.info("Running benchmark on same core, same thread")
 
-    run_two_containers(f"taskset {SAME_CORE}", f"taskset {SAME_CORE}", "same-thread", run_index)
+    run_two_containers(
+        f"taskset {MAIN_CORE}", f"taskset {MAIN_CORE}", "same-thread", run_index, instances_per_benchmark
+    )
+    clean_all_containers()
+    time.sleep(5)
+
+    run_two_containers(
+        f"taskset {MAIN_CORE}", f"taskset {SAME_CORE}", "same-core", run_index, instances_per_benchmark
+    )
     log.info("Benchmark completed")
--- benchmark_cli/experiments/performance_isolation/unikraft_redis_perf_iso.py
+++ benchmark_cli/experiments/performance_isolation/unikraft_redis_perf_iso.py
@@ -8,23 +8,36 @@ EXPERIMENT_NAME = "uk_redis_perf_iso"
 SAME_CORE = 1
 BUSY_VM_NAME = "busy"
 
+CORE_27 = "0x8000000"
+CORE_79 = "0x80000000000000000000"  # Same Core as hyperthread 27
+CORE_85 = "0x2000000000000000000000"  # Same CPU as hyperthread 27
+CORE_15 = "0x8000"  # Different CPU node
 
-def run_two_unikrafts(taskset_1, taskset_2, file_name, run_index):
+
+def run_two_unikrafts(main_taskset, attack_taskset, file_name, run_index, instances_per_benchmark):
     run_unikraft(
         ip_address="172.16.0.2",
         instance_cnt=1,
         name="redis",
-        taskset_text=taskset_1,
+        taskset_text=main_taskset,
     )
 
     run_unikraft(
         ip_address=None,
         instance_cnt=None,
         name=BUSY_VM_NAME,
-        taskset_text=taskset_2,
+        taskset_text=attack_taskset,
     )
-    time.sleep(25)
-    run_redis_benchmark("172.16.0.2", 6379, f"benchmark-data/{EXPERIMENT_NAME}/{run_index}-{file_name}.out")
+    time.sleep(5)
+
+    for i in range(instances_per_benchmark):
+        log.info(f"benchmark-data/{EXPERIMENT_NAME}/{(run_index - 1) * instances_per_benchmark + i}-{file_name}.out")
+        run_redis_benchmark(
+            "172.16.0.2",
+            6379,
+            f"benchmark-data/{EXPERIMENT_NAME}/{(run_index - 1) * instances_per_benchmark + i}-{file_name}.out"
+        )
+        time.sleep(1)
 
 
 def run_unikraft_redis_perf_iso_experiment(run_index, benchmark_times, instances_per_benchmark):
@@ -42,13 +55,26 @@ def run_unikraft_redis_perf_iso_experiment(run_index, benchmark_times, instances
     time.sleep(25)
     log.info("Main VM started. Starting first wrk benchmark")
 
-    run_redis_benchmark("172.16.0.2", 6379, f"benchmark-data/{EXPERIMENT_NAME}/{run_index}-single.out")
+    for i in range(instances_per_benchmark):
+        log.info(f"benchmark-data/{EXPERIMENT_NAME}/{(run_index - 1) * instances_per_benchmark + i}-single.out")
+        run_redis_benchmark(
+            "172.16.0.2",
+            6379,
+            f"benchmark-data/{EXPERIMENT_NAME}/{(run_index - 1) * instances_per_benchmark + i}-single.out"
+        )
+        time.sleep(1)
+
     log.info("Single run completed")
 
     clean_all_vms()
-    time.sleep(10)
+    time.sleep(5)
 
     log.info("Running benchmark on same core, same thread")
 
-    run_two_unikrafts(f"taskset {SAME_CORE}", f"taskset {SAME_CORE}", "same-thread", run_index)
+    run_two_unikrafts(f"taskset {CORE_27}", f"taskset {CORE_27}", "same-thread", run_index, instances_per_benchmark)
+
+    clean_all_vms()
+    time.sleep(5)
+
+    run_two_unikrafts(f"taskset {CORE_27}", f"taskset {CORE_79}", "same-core", run_index, instances_per_benchmark)
     log.info("Benchmark completed")
